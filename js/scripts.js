e1a5db127de5c79a0f585d82388b13306f1c4677
Jon Stump <jmstump@gmail.com>
2021-01-13 13:22:58 -0800

    add calculator business logic.
    Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-function add(number1, number2) {
+//Business Logic:
+
+const add = function add(number1, number2) {
 	return number1 + number2;
 }
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is user interface logic
+
+$(document).ready(function() {
+	const number1 = parseInt(prompt("Enter a number:"));
+	const number2 = parseInt(prompt("Enter another number:"));
+
+	alert(add(number1, number2));
+});
