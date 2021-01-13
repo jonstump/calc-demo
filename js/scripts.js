ad4bff6c16539f5bfc3b02d9af35c10dfcb31a3b
Jon Stump <jmstump@gmail.com>
2021-01-13 14:13:23 -0800

    add subtract, multiply, and divide to our calculator.
    Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,32 @@ const divide = function(number1, number2) {
 // Everything below this line is user interface logic
 
 $(document).ready(function() {
-	const number1 = parseInt(prompt("Enter a number:"));
-	const number2 = parseInt(prompt("Enter another number:"));
-
-	alert(add(number1, number2));
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#add1").val());
+		const number2 = parseInt($("#add2").val());
+		const result = add(number1, number2);
+		$("#outputAdd").text(result);	
+	});
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#subtract1").val());
+		const number2 = parseInt($("#subtract2").val());
+		const result = subtract(number1, number2);
+		$("#outputSubtract").text(result);	
+	});
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#multiply1").val());
+		const number2 = parseInt($("#multiply2").val());
+		const result = multiply(number1, number2);
+		$("#outputMultiply").text(result);	
+	});
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#divide1").val());
+		const number2 = parseInt($("#divide2").val());
+		const result = divide(number1, number2);
+		$("#outputDivide").text(result);	
+	});
 });
